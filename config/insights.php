4823e0deeda3bf97bafaca423d62c7863e88bcce
Samuel Ryan <sam@samryan.co.uk>
2020-09-10 01:49:27 +0100

    :wrench: Increase minimum allowed architecture and quality scores

--- config/insights.php
+++ config/insights.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+
 return [
     'requirements' => [
-        'min-architecture' => 95,
+        'min-architecture' => 100,
         'min-complexity' => 95,
-        'min-quality' => 95,
+        'min-quality' => 100,
         'min-style' => 100,
+    ],
+    'config' => [
+        OrderedImportsFixer::class => [
+            'imports_order' => ['class', 'const', 'function'],
+            'sort_algorithm' => 'alpha',
+        ],
     ],
 ];

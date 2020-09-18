15ad1f90ad6c95f794ba498b0ae17c5a3d1b0d22
Samuel Ryan <sam@samryan.co.uk>
2020-09-18 18:07:13 +0100

    📦 Exclude non-application project files (#27)

--- config/view.php
+++ config/view.php
@@ -6,8 +6,5 @@ return [
     'paths' => [
         resource_path('views'),
     ],
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
-    ),
+    'compiled' => realpath(storage_path('framework/views')),
 ];
